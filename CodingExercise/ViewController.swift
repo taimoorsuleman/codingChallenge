bd1b16e40c8ac41374763ee273a617fd437a5a0c
Taimoor Suleman <taimoor.suleman@systemsltd.com>
2023-11-11 21:04:17 +0500

    Initial Commit

---
+++ CodingExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodingExercise
+//
+//  Created by Taimoor Suleman on 11/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
